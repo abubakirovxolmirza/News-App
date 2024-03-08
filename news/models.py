6ef72704d065d6369b49454d472de886d2a12799
abubakirovxolmirza <abubakirovxolmirza1@gmail.com>
2024-03-08 18:11:37 +0500

    Added: test, Updated models.py

--- news/models.py
+++ news/models.py
@@ -1,13 +1,11 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth.models import User
+# Create your models here.
 
 
 class News(models.Model):
     user = models.ForeignKey(User, models.CASCADE)
-    image = models.ImageField(upload_to='news_img')
+    image = models.ImageField(upload_to='news_img', blank=True)
     title = models.CharField(max_length=128)
     content = models.TextField()
     post_date = models.DateTimeField(auto_now_add=True)
@@ -16,11 +14,6 @@ class News(models.Model):
     class Meta:
         db_table = 'News'
 
-    def __str__(self):
-        if len(self.title) > 40:
-            return f"{self.title[:40]}..."
-        return self.title
-
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -28,7 +21,4 @@ class Comment(models.Model):
     comment = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        if len(self.comment) > 40:
-            return f"{self.comment[:40]}..."
-        return self.comment
+
--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
+from .models import News, Comment
 # Create your tests here.
+
+
+class NewsModelTest(TestCase):
+
+    def setUp(self):
+
+        self.user = User.objects.create(username='tester1', email='shokh3332@gmail.com', password='tester12.')
+        self.news = News.objects.create(
+            user=self.user,
+            title='Test Title',
+            content='Test Content'
+        )
+        self.comment = Comment.objects.create(user=self.user, news=self.news, comment='Test Comment')
+
+    def test_title(self):
+        obj = News.objects.get(id=self.news.id)
+
+        self.assertEqual(obj.title, 'Test Title')
+
+    def test_content(self):
+        obj = News.objects.get(id=self.news.id)
+
+        self.assertEqual(obj.content, 'Test Content')
